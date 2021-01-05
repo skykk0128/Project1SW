1b7f8dfd7e21c3b90185a86e4b683a55893d49b0
Steven Wang <skykk0128@gmail.com>
2021-01-05 20:41:46 +1100

    New Changes made on Project 1 css

---
+++ src/js/javascript.js
@@ -0,0 +1,73 @@
+'use strict';
+
+// Highlight menu item when navigating
+document.getElementById("homepage").addEventListener("click", event => {
+  
+  document.getElementById("homepage").classList.add("current");
+  document.getElementById("aboutpage").classList.remove("current");
+  document.getElementById("resumepage").classList.remove("current");
+  document.getElementById("projectspage").classList.remove("current");
+  document.getElementById("contactpage").classList.remove("current");
+  document.getElementById("blogpage").classList.remove("current");
+});
+document.getElementById("aboutpage").addEventListener("click", event => {
+  document.getElementById("homepage").classList.remove("current");
+  document.getElementById("aboutpage").classList.add("current");
+  document.getElementById("aboutpage").classList.add("active");
+  document.getElementById("resumepage").classList.remove("current");
+  document.getElementById("projectspage").classList.remove("current");
+  document.getElementById("contactpage").classList.remove("current");
+  document.getElementById("blogpage").classList.remove("current");
+});
+document.getElementById("resumepage").addEventListener("click", event => {
+  document.getElementById("homepage").classList.remove("current");
+  document.getElementById("aboutpage").classList.remove("current");
+  document.getElementById("resumepage").classList.add("current");
+  document.getElementById("projectspage").classList.remove("current");
+  document.getElementById("contactpage").classList.remove("current");
+  document.getElementById("blogpage").classList.remove("current");
+});
+document.getElementById("projectspage").addEventListener("click", event => {
+  document.getElementById("homepage").classList.remove("current");
+  document.getElementById("aboutpage").classList.remove("current");
+  document.getElementById("resumepage").classList.remove("current");
+  document.getElementById("projectspage").classList.add("current");
+  document.getElementById("contactpage").classList.remove("current");
+  document.getElementById("blogpage").classList.remove("current");
+});
+document.getElementById("contactpage").addEventListener("click", event => {
+  document.getElementById("homepage").classList.remove("current");
+  document.getElementById("aboutpage").classList.remove("current");
+  document.getElementById("resumepage").classList.remove("current");
+  document.getElementById("projectspage").classList.remove("current");
+  document.getElementById("contactpage").classList.add("current");
+  document.getElementById("blogpage").classList.remove("current");
+});
+document.getElementById("blogpage").addEventListener("click", event => {
+  document.getElementById("homepage").classList.remove("current");
+  document.getElementById("aboutpage").classList.remove("current");
+  document.getElementById("resumepage").classList.remove("current");
+  document.getElementById("projectspage").classList.remove("current");
+  document.getElementById("contactpage").classList.remove("current");
+  document.getElementById("blogpage").classList.add("current");
+});
+
+// Changeable Position
+var swiper = new Swiper('.swiper-container', {
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  loop: true,
+});
+
+// Sticky Navigation Bar
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+window.onscroll = () => {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+};
