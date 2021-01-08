fbde6ea7c30881b39aa496fdb4d59ec92db2880b
Steven Wang <skykk0128@gmail.com>
2021-01-09 00:20:46 +1100

    New changes added to Project 1

--- src/js/javascript.js
+++ src/js/javascript.js
@@ -86,7 +86,19 @@ window.onscroll = () => {
 //   }
 // });
 
-
+// Contact Map
+(function () {
+    var setting = {"height":200,"width":400,"zoom":20,"queryString":"Carnegie VIC, Australia","place_id":"ChIJb4uHlo5p1moRQNyMIXVWBAU","satellite":false,"centerCoord":[-37.89376058556284,145.05607450719017],"cid":"0x5045675218cdc40","lang":"en","cityUrl":"/australia/carnegie-60300","cityAnchorText":"Map of Carnegie, Victoria, Australia","id":"map-9cd199b9cc5410cd3b1ad21cab2e54d3","embed_id":"341002"};
+    var d = document;
+    var s = d.createElement('script');
+    s.src = 'https://1map.com/js/script-for-user.js?embed_id=341002';
+    s.async = true;
+    s.onload = function (e) {
+      window.OneMap.initMap(setting)
+    };
+    var to = d.getElementsByTagName('script')[0];
+    to.parentNode.insertBefore(s, to);
+})();
 
 
 $(window).load(function() {
